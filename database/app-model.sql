a9a0628e5d779e92289b2fe1e2d9e7a9791bcf0a
Ulysses de Aguilar <ulyssesdeaguilar@gmail.com>
2024-06-12 11:06:39 +0200

    feat: New scenarios!

--- database/app-model.sql
+++ database/app-model.sql
@@ -53,3 +53,64 @@ VALUES
             "1000"
         )
     );
+
+
+INSERT INTO
+    app_models(name, package, interface)
+VALUES
+    (
+        "FactoryApp",
+        "simcan2.Applications.UserApps.FactoryApp",
+        JSON_OBJECT(
+            "listeningPort",
+            "int"
+        )
+    ),
+    (
+        "FarmApp",
+        "simcan2.Applications.UserApps.FarmApp",
+        JSON_OBJECT(
+            "listeningPort",
+            "int"
+        )
+    ),
+    (
+        "BuildingApp",
+        "simcan2.Applications.UserApps.BuildingApp",
+        JSON_OBJECT(
+            "listeningPort",
+            "int"
+        )
+    );
+
+INSERT INTO
+    apps(name, model_id, parameters)
+VALUES
+    (
+        "FactoryApp",
+        4,
+        JSON_OBJECT(
+            "listeningPort",
+            "1000"
+        )
+    ),
+    (
+        "FarmApp",
+        5,
+        JSON_OBJECT(
+            "listeningPort",
+            "1000"
+        )
+    ),
+    (
+        "BuildingApp",
+        6,
+        JSON_OBJECT(
+            "listeningPort",
+            "1000"
+        )
+    );
+
+-- Confirmation query
+SELECT apps.name, app_models.name
+FROM apps JOIN app_models ON apps.model_id = app_models.id;
