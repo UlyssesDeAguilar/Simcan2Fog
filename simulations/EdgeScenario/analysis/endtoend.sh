d73fd607dc09c93ebd88e3e47c6f45277d79dea5
Ulysses de Aguilar <ulyssesdeaguilar@gmail.com>
2024-06-13 09:02:06 +0200

    feat: More statistical analysis

--- simulations/EdgeScenario/analysis/endtoend.sh
+++ simulations/EdgeScenario/analysis/endtoend.sh
@@ -2,6 +2,6 @@
 RDIR=../results
 
 # TargetDc -- Focus onto the single controller
-scavetool query -l -f 'attr:configname("targetDc") AND module("**.app") AND name("endToEndDelay*")' $RDIR/*.vec $RDIR/*.sca
-#scavetool export -f 'attr:configname("targetDc") AND module("**.app") AND name(endToEndDelay:vector)' -F CSV-R -o targetDc.csv $RDIR/*.vec
-#scavetool export -f 'attr:configname("targetFog") AND module("**.app") AND name(endToEndDelay:vector)' -F CSV-R -o targetFog.csv $RDIR/*.vec
+#scavetool query -l -f 'attr:configname("targetDc") AND module("**.app") AND name("endToEndDelay*")' $RDIR/*.vec $RDIR/*.sca
+scavetool export -f 'attr:configname("targetDc") AND module("**.app") AND name(endToEndDelay:vector)' -F CSV-R -o targetDcDelay.csv $RDIR/*.vec
+scavetool export -f 'attr:configname("targetFog") AND module("**.app") AND name(endToEndDelay:vector)' -F CSV-R -o targetFogDelay.csv $RDIR/*.vec
