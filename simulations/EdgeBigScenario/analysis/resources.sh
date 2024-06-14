eab5c787e64f23a8e2bdd643897ca3f8af1c74ca
Ulysses de Aguilar <ulyssesdeaguilar@gmail.com>
2024-06-14 22:08:56 +0200

    feat: Statistical analysis of resources for EdgeBigScenario

--- simulations/EdgeBigScenario/analysis/resources.sh
+++ simulations/EdgeBigScenario/analysis/resources.sh
@@ -4,9 +4,10 @@ RDIR=../results
 # TargetDc
 # Focus only onto the dc allocated resources
 ## scavetool query -l -f 'attr:configname("targetDc") AND module("**.dc.resourceManager") AND name("allocated*:vector")' $RDIR/*.vec $RDIR/*.sca
-#scavetool export -f 'attr:configname("targetDc") AND module("**.dc.resourceManager") AND name("allocated*:vector")' -F CSV-R -o targetDc.csv $RDIR/*.vec
+scavetool export -f 'attr:configname("targetDc") AND module("**.dc.resourceManager") AND name("allocated*:vector")' -F CSV-R -o targetDcRes.csv $RDIR/*.vec
 
 # TargetFog
 # Export all fog resources - might be interesing to isolate by region
 ## scavetool query -l -f 'attr:configname("targetFog") AND module("**.fg*.resourceManager") AND name("allocated*:vector")' $RDIR/*.vec $RDIR/*.sca
-scavetool export -f 'attr:configname("targetFog") AND module("**.fg*.resourceManager") AND name("allocated*:vector")' -F CSV-R -o targetFog.csv $RDIR/*.vec
+scavetool export -f 'attr:configname("targetFog") AND module("**.fg*.resourceManager") AND name("allocated*:vector")' -F CSV-R -o targetFogRes1.csv $RDIR/*.vec
+scavetool export -f 'attr:configname("targetFog") AND module("**.dc.resourceManager") AND name("allocated*:vector")' -F CSV-R -o targetFogRes2.csv $RDIR/*.vec
