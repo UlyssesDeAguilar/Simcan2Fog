e0f022bdd53480671b769a2c97fa8b05f929a3c3
Ulysses de Aguilar <ulyssesdeaguilar@gmail.com>
2024-06-26 16:41:13 +0200

    feat: Adapted setup script

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 # It's pretty generic if you have the same "common base"
 export omnetpp_root=$HOME/omnetpp-5.6.2
 export PATH=$omnetpp_root/bin:$PATH
-export SIMCAN_HOME=$omnetpp_root/Simcan2Cloud
+export SIMCAN_HOME=$omnetpp_root/Simcan2Fog
 export INET_HOME=$omnetpp_root/inet4
