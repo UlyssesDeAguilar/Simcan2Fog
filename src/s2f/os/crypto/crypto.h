1130a2b002ca2e52db0fde2809fa228f08f50272
TomasDanielExpositoTorre <expositotomas2002@gmail.com>
2025-11-26 16:47:20 +0100

    style: moved template methods to the bottom of the header

--- src/s2f/os/crypto/crypto.h
+++ src/s2f/os/crypto/crypto.h
@@ -50,66 +50,6 @@ namespace s2f::os::crypto
     // Methods
     //-------------------------------------------------------------------------
 
-    /** * Computes a sha256 hash of the given data.
-     *
-     * @param data Data to hash.
-     * @param size Data size.
-     *
-     * @return The hash value as a std::array.
-     */
-    template <typename T>
-    sha256digest sha256(const T *data, size_t size)
-    {
-        sha256digest hash;
-        auto d = reinterpret_cast<const unsigned char *>(data);
-        auto h = reinterpret_cast<unsigned char *>(hash.data());
-        SHA256(d, size, h);
-        return hash;
-    }
-
-    /**
-     * Computes the double sha256 hash of the given data.
-     *
-     * @param data Data to hash.
-     * @param size Data size.
-     *
-     * @return The hash value.
-     */
-    template <typename T>
-    sha256digest dsha256(const T *data, size_t size)
-    {
-        return sha256(sha256(data, size).data(), SHA256_DIGEST_LENGTH);
-    }
-
-    /**
-     * Computes a ripemd160 hash of the given data.
-     *
-     * Mostly used to reduce public key size in bitcoin.
-     *
-     * @param data Data to hash.
-     * @param size Data size.
-     *
-     * @return The hash value as a std::array.
-     */
-    template <typename T>
-    ripemd160digest ripemd160(const T *data, size_t size)
-    {
-        ripemd160digest hash;
-        EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-        unsigned int md_len = RIPEMD160_DIGEST_LENGTH;
-        const EVP_MD *md = EVP_get_digestbyname("RIPEMD160");
-
-        auto d = reinterpret_cast<const unsigned char *>(data);
-        auto h = reinterpret_cast<unsigned char *>(hash.data());
-
-        EVP_DigestInit_ex2(mdctx, md, nullptr);
-        EVP_DigestUpdate(mdctx, d, size);
-        EVP_DigestFinal_ex(mdctx, h, &md_len);
-
-        EVP_MD_CTX_free(mdctx);
-        return hash;
-    }
-
     /**
      * Appends arbitrary data into a byte array.
      *
@@ -166,5 +106,65 @@ namespace s2f::os::crypto
      * @returns true if the signatures match, false otherwise.
      */
     bool verify(const key &pub, bytes &data, bytes &signature);
+
+    /**
+     * Computes a ripemd160 hash of the given data.
+     *
+     * Mostly used to reduce public key size in bitcoin.
+     *
+     * @param data Data to hash.
+     * @param size Data size.
+     *
+     * @return The hash value as a std::array.
+     */
+    template <typename T>
+    ripemd160digest ripemd160(const T *data, size_t size)
+    {
+        ripemd160digest hash;
+        EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+        unsigned int md_len = RIPEMD160_DIGEST_LENGTH;
+        const EVP_MD *md = EVP_get_digestbyname("RIPEMD160");
+
+        auto d = reinterpret_cast<const unsigned char *>(data);
+        auto h = reinterpret_cast<unsigned char *>(hash.data());
+
+        EVP_DigestInit_ex2(mdctx, md, nullptr);
+        EVP_DigestUpdate(mdctx, d, size);
+        EVP_DigestFinal_ex(mdctx, h, &md_len);
+
+        EVP_MD_CTX_free(mdctx);
+        return hash;
+    }
+
+    /** * Computes a sha256 hash of the given data.
+     *
+     * @param data Data to hash.
+     * @param size Data size.
+     *
+     * @return The hash value as a std::array.
+     */
+    template <typename T>
+    sha256digest sha256(const T *data, size_t size)
+    {
+        sha256digest hash;
+        auto d = reinterpret_cast<const unsigned char *>(data);
+        auto h = reinterpret_cast<unsigned char *>(hash.data());
+        SHA256(d, size, h);
+        return hash;
+    }
+
+    /**
+     * Computes the double sha256 hash of the given data.
+     *
+     * @param data Data to hash.
+     * @param size Data size.
+     *
+     * @return The hash value.
+     */
+    template <typename T>
+    sha256digest dsha256(const T *data, size_t size)
+    {
+        return sha256(sha256(data, size).data(), SHA256_DIGEST_LENGTH);
+    }
 }
 #endif
